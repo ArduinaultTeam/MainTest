179e8bbf2222ce38261d5ed17474e772689a0284
Arhimeas <arhimeas@gmail.com>
2016-11-28 19:13:04 +0100

    Add files via upload

---
+++ Jeremy/_3_servo_controled_by_IR_remote_off_function_MBM.ino
@@ -0,0 +1,89 @@
+#include<IRremote.h>
+IRrecv receiver(11);
+decode_results results;
+
+#include<Servo.h>
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+int CASE = 0;
+int servopos1=20;
+int servopos2=0;
+int inity=0;
+int etat=0;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+receiver.enableIRIn();
+
+servo1.attach(5);
+servo2.attach(4);
+servo3.attach(3);
+pinMode(13,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(receiver.decode(&results)){
+  int val= results.value;
+  Serial.println(val,HEX);
+inity=1;
+  switch(val)
+  {
+    case 0xFF629D:
+    CASE=1;
+    break;
+
+    case 0xFFA857:
+    CASE=2;
+    break;
+
+    case 0xFF22DD:
+    CASE=3;
+    break;
+
+    case 0xFFC23D:
+    CASE=4;
+    break;
+
+    case 0xFF4AB5:
+    CASE=5;
+    if (etat==0)
+    etat=1;
+    if(etat==1)
+    etat=0;
+    break;
+
+
+
+    case 0xFFFFFFFF:
+    break;
+  }
+  if(etat==0)
+  {
+    digitalWrite(13,HIGH);
+  switchCaseSimplify(CASE);
+  servo1.write(servopos1);
+  servo2.write(servopos2);
+  servo3.write(servopos2);
+  }
+  else
+  {
+    digitalWrite(13,LOW);
+     servo1.write(40);
+    servo2.write(40);
+    servo3.write(40);
+  }
+  receiver.resume();
+delay(100);
+}
+if(inity==0)
+  {
+    servo1.write(20);
+    servo2.write(0);
+    servo3.write(0);
+  }
+}
---
+++ Jeremy/switch_case_simplify.ino
@@ -0,0 +1,31 @@
+void switchCaseSimplify(int val){
+  
+  switch(val)
+  {
+    case 0:
+    break;
+
+    case 1:
+    if(servopos1<160)
+    servopos1+=5;
+    break;
+    
+     case 2:
+    if(servopos1>10)
+    servopos1-=5;
+    break;
+
+     case 3:
+    if(servopos2<80)
+    servopos2+=5;
+    break;
+
+     case 4:
+    if(servopos2>5)
+    servopos2-=5;
+    break;
+
+   case 5:
+   break;
+  }
+}
